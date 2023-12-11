73284b2c03c1933b1cc421aa28c697041b365789
MadhumithaUppu <100417568+MadhumithaUppu@users.noreply.github.com>
2023-12-11 02:28:26 -0500

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-
+// We need to include all the packages needed for the script to work
 #[macro_use] extern crate rocket;
 use rocket::form::Form;
 use rocket_dyn_templates::Template;
@@ -9,7 +9,7 @@ use rust_iptables::iptables::IPTables;
 struct BlockIp {
     ip: String,
 }
-
+// the below function is used to get the ip address that is used for blocking 
 #[post("/block_ip", data = "<block_ip>")]
 fn block_ip_route(block_ip_form: Form<BlockIp>) -> String {
     let block_ip = &block_ip_form.ip;
@@ -27,7 +27,7 @@ fn block_ip(ip: &str) -> Result<(), Box<dyn Error>> {
 
     Ok(())
 }
-
+// below  I have integrated a route handler to keep the ip tp be blocked
 #[get("/")]
 fn index() -> Template {
     let context = HashMap::<String, String>::new();
@@ -39,6 +39,7 @@ async fn main() {
     let _ = rocket().launch().await;
 }
 
+// Below funtion is used to crate the rocket instace to run the web server to give the ui to set the ip address to be blocked
 fn rocket() -> rocket::Rocket<rocket::Build> {
     rocket::build()
         .mount("/", routes![index, block_ip_route])
